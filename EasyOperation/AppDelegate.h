3a39fb40547cb32da330569d62f1c1cb0ed95a0b
ccSunday <935143023@qq.com>
2018-03-27 09:00:11 +0800

    initial

---
+++ EasyOperation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyOperation
+//
+//  Created by ccSunday on 2018/3/27.
+//  Copyright © 2018年 ccSunday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
