f1f6a7eb27f3afa80b39ff406bf607105c6fd6c9
ccSunday <935143023@qq.com>
2018-04-05 18:05:18 +0800

    Operation基本操作

--- EasyOperation/ViewController.m
+++ EasyOperation/ViewController.m
@@ -31,9 +31,15 @@ typedef void (^Operationtask)(void);
 
 @interface ViewController ()
 
+@property (nonatomic,assign) int ticketCount;
+
 @end
 
 @implementation ViewController
+{
+    NSLock *_lock;
+    NSMutableArray *_ticketsArray;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -52,7 +58,13 @@ typedef void (^Operationtask)(void);
 //    [self testOperationQueue];
     
     //5、addOperationWithBlockToQueue
-    [self addOperationWithBlockToQueue];
+//    [self addOperationWithBlockToQueue];
+    
+    //6、test Thread Communication
+//    [self testThreadCommunication];
+    
+    //7、testThreadSafe
+    [self testThreadSafe];
     
 }
 
@@ -223,52 +235,134 @@ typedef void (^Operationtask)(void);
     NSLog(@"op3");
 }
 
-#pragma mark 使用 addOperationWithBlock: 将操作加入到操作队列中
+#pragma mark 使用 addOperationWithBlock: 将操作加入到操作队列中||测试线程依赖
 - (void)addOperationWithBlockToQueue{
     //创建一个队列，以便向里面添加任务，添加一些block到数组中
     NSMutableArray *blockArr = [NSMutableArray array];
     //创建任务0，栈中的block会被自动拷贝到堆中
-    
     Operationtask task0 = ^(void){
-        [NSThread sleepForTimeInterval:5];
+        [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
         NSLog(@"task===0");
     };
-    
     //创建任务1
     Operationtask task1  = ^(void){
-        [NSThread sleepForTimeInterval:2];
+        [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
         NSLog(@"task===1");
 
     };
-  
     //创建任务2
     Operationtask task2 = ^(void){
-        [NSThread sleepForTimeInterval:2];
+        [NSThread sleepForTimeInterval:3]; // 模拟耗时操作
         NSLog(@"task===2");
     };
   
     //创建void任务
     void(^taskVoid)(void) = ^(void){
-        [NSThread sleepForTimeInterval:2];
+        [NSThread sleepForTimeInterval:4]; // 模拟耗时操作
         NSLog(@"task===3");
     };
     
-    [blockArr addObjectsFromArray:@[task0,task2,taskVoid,task1]];
+    [blockArr addObjectsFromArray:@[task0,task1,task2,taskVoid]];
     
     NSLog(@"operations:%@",blockArr);
     
     NSOperationQueue *queue = [[NSOperationQueue alloc]init];
-    
+    /*
+     maxConcurrentOperationCount:
+     为1的时候串行同步执行
+     为>1的时候并发异步执行
+     
+     */
+    queue.maxConcurrentOperationCount = 1;
     for (NSUInteger i = 0; i<blockArr.count; i++) {
         Operationtask task = blockArr[i];
         NSBlockOperation *curBlockOperation = [NSBlockOperation blockOperationWithBlock:task];
         if (i<blockArr.count-1) {
             NSBlockOperation *nextBlockOperation = [NSBlockOperation blockOperationWithBlock:blockArr[i+1]];
-            //只能简单的起到顺序执行的作用，
+            //当添加依赖的时候，会在方法调用的时候起到顺序执行的作用，并且会等到之前的操作执行完成后，才会执行接下来的任务，从而达到依赖的作用，在前面的completion代码块中，调用后面的operation，operation完成后，调用completion代码块，不断循环。
             [nextBlockOperation addDependency:curBlockOperation];
         }
         [queue addOperation:curBlockOperation];
     }
+}
+
+#pragma mark 线程通讯
+- (void)testThreadCommunication{
+    NSOperationQueue *queue = [[NSOperationQueue alloc]init];
+    //子线程与主线程通讯
+    [queue addOperationWithBlock:^{
+        NSLog(@"thread start operation");
+        [NSThread sleepForTimeInterval:3];
+        NSLog(@"thread dispatch to main");
+        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"update mainThread operations");
+            self.view.backgroundColor = [UIColor redColor];
+        }];
+    }];
+}
+
+#pragma mark 线程安全,模拟三个窗口同时卖票，多个线程同时更新数据
+- (void)testThreadSafe{
+    //数据初始化
+    _ticketCount = 50;
+    _ticketsArray = [NSMutableArray array];
+    _lock = [[NSLock alloc]init];
+    
+    __weak typeof(self)weakSelf = self;
+    //创建队列与操作
+    
+    //创建queue1
+    NSOperationQueue *queue1 = [[NSOperationQueue alloc]init];
+    //创建任务1
+    NSBlockOperation *operation1 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+    
+    //创建queue2
+    NSOperationQueue *queue2 = [[NSOperationQueue alloc]init];
+    
+    //创建任务2
+    NSBlockOperation *operation2 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+    
+    //创建queue3
+    NSOperationQueue *queue3 = [[NSOperationQueue alloc]init];
+    //创建任务3
+    NSBlockOperation *operation3 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+    [queue1 addOperation:operation1];
+    [queue2 addOperation:operation2];
+    [queue3 addOperation:operation3];
+}
+
+
+- (void)saleTicketSafe{
+    while (1) {
+        //加锁
+        [_lock lock];//加锁的话会变为同步，变慢，不加锁很快。
+        if (self.ticketCount>0) {
+            _ticketCount--;
+            NSLog(@"剩余票数:%d,窗口：%@",_ticketCount,[NSThread currentThread]);
+            [NSThread sleepForTimeInterval:0.2];
+        }
+        //解锁
+        [_lock unlock];
+        if (self.ticketCount<=0) {
+            NSLog(@"所有火车票已售完");
+            break;
+        }
+    }
+}
+
+- (void)writeToDataSourceSafe{
+    
+}
+
+- (void)deleteDataSourceSafe{
+    
 }
 
 - (void)didReceiveMemoryWarning {
